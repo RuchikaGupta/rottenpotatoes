f08091efb4c305a66f2117338ae2be7b17a637ed
saasbook <saasbook@saasbook.(none)>
2013-10-03 23:05:07 -0700

    first working version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,38 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    should_redirect = false
+    @all_ratings = Movie.all_ratings 
+    if params[:ratings]
+      @ratings = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      should_redirect = true
+    else
+      @ratings = {}
+      Movie.all_ratings.each do |rating|
+        @ratings[rating]=1
+      end
+      should_redirect = true
+    end
+
+    if params[:sort]
+      @sort = params[:sort]
+    elsif session[:sort]
+      @sort = session[:sort]
+      should_redirect = true
+    end
+    if should_redirect
+      redirect_to movies_path(:ratings=>@ratings, :sort=>@sort)
+    end
+    @movies = [] 
+    Movie.all(:order=>@sort ? @sort : :id).each do |movie|
+      if @ratings.keys.include?(movie[:rating]) 
+        @movies.append(movie)
+      end
+    end
+    session[:ratings] = @ratings
+    session[:sort] = @sort
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+     all = self.all
+     ratings = all(:select=>"rating",:group=>"rating").map(&:rating)
+  end
 end
